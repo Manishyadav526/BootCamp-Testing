1d786c84974aecb3a704da83992ca64cf2e5b347
Manish Yadav <manishyadav92124@gmail.com>
2026-07-22 20:45:36 +0530

    Add files via upload

---
+++ Automation_Testing_Selenium/18-07-2026/Drag_and_Drop_Action/src/test/java/com/Selenium_WebDriver_Actions/DragAndDropConcept.java
@@ -0,0 +1,32 @@
+package com.Selenium_WebDriver_Actions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class DragAndDropConcept {
+
+    public static void main(String[] args) throws Exception {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+
+        driver.get("http://jqueryui.com/droppable/");
+        Thread.sleep(4000);
+
+        // Switch to frame if it's present
+        driver.switchTo().frame(0);
+
+        Actions action = new Actions(driver);
+
+        action.clickAndHold(driver.findElement(By.xpath("//*[@id='draggable']")))
+              .moveToElement(driver.findElement(By.xpath("//*[@id='droppable']")))
+              .release()
+              .build()
+              .perform();
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
